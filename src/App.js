11bd5665c9b5c8a67f756975fe53492a5fb10a69
MatheusHiro <hirofifa13@gmail.com>
2024-01-10 22:26:47 -0300

    Adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <aside className='App-menu'>
+        <ul className='App-menu-list'>
+          <li>
+            <a className='App-menu-list-item-link' href='https://www.linkedin.com/in/matheus-hiroyuki-suwa-moura-32bba0209/'>
+              linkedin
+            </a>
+          </li>
+
+        </ul>
+
+
+      </aside>
     </div>
   );
 }
